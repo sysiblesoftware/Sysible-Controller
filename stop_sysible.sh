527dd287d92026a330b3bc348ed8dbab7ceb6952
Sysible Enterprise Software <sysiblesoftware@gmail.com>
2026-06-20 12:40:55 -0400

    Remove legacy stop_sysible.sh shim, superseded by sysible_controller stop

--- stop_sysible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Replaced by the `sysible_controller` CLI (installed to /usr/local/bin
-# by install_sysible.sh) - see start_sysible.sh for why. Kept as a thin
-# redirect so old habits/automation pointed at this path still work.
-echo "stop_sysible.sh has been replaced by the 'sysible_controller' command."
-echo "Running: sudo sysible_controller stop"
-exec sysible_controller stop "$@"
